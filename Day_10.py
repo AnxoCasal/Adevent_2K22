42c63e44ec5e945b489ab0a926a0764dd2149f1a
AnxoCasal <anxocasal2002@gmail.com>
2023-12-11 17:30:27 +0100

    Completado 10_1

--- Day_10.py
+++ Day_10.py
@@ -2,28 +2,25 @@ with open("clock_program.txt") as file:
     lines = file.readlines()
     
 cycle = 0
-x = 0
+x = 1
 mid_cycle = False
 values = []
+i = 0
+last_value = 0
+
+while True:
+    cycle += 1
+    if not mid_cycle:
+        i+=1
     
-for i in range(len(lines)):
-    lines[i] = lines[i].strip()
-    
-    cycle+=1
+    if cycle == 20 or cycle == 60 or cycle == 100 or cycle == 140 or cycle == 180 or cycle == 220:
+        values.append(cycle * x)
     
     if mid_cycle:
         mid_cycle = False
-    elif lines[i] != "noop":
-        lines[i] = lines[i].replace("addx ","")
-        x += int(lines[i])
+        x += last_value
+    elif lines[i].strip() != "noop":
+        last_value = int(lines[i][4:].strip())
         mid_cycle = True
-        
-    if cycle == 20 or cycle == 60 or cycle == 100 or cycle == 140 or cycle == 180 or cycle == 220:
-        print(cycle)
-        values.append(cycle * x)        
-                
-print(sum(values))
-
-#17380 > ?
-
-### NO ME ESTOY PARANDO CUANDO HAY CICLOS EXTRAS
+    
+    if i == len(lines)-1: break
